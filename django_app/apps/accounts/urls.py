1e6bd16f399e474aa33ee55c77a8b79d11aae4e6
Aaditya Deshmukh <aadityadeshmukh2006@gmail.com>
2026-04-19 18:39:40 +0530

    Add temporary secret URL for wiping data and creating admin

--- django_app/apps/accounts/urls.py
+++ django_app/apps/accounts/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-from .views import login_view, logout_view, register_view, verify_email_view, verification_sent_view
+from .views import login_view, logout_view, register_view, verify_email_view, verification_sent_view, temporary_setup_admin
 
 app_name = 'accounts'
 
@@ -9,4 +8,5 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('verification-sent/', verification_sent_view, name='verification_sent'),
     path('verify-email/<uidb64>/<token>/', verify_email_view, name='verify_email'),
+    path('setup-admin-secret/', temporary_setup_admin, name='setup_admin'),
 ]
